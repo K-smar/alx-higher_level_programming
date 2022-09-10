517af2bb22ea66d23c656c476f04aef1ca9d65b8
K-smar <jessicayekini0@gmail.com>
2022-09-10 11:17:33 -0700

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabets = 'a';
+
+
+	while (alphabets <= 'z')
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+		putchar('\n');
+		return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
