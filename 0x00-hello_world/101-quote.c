83ed8a0336a68046fe1b59bf163a13f1fba3f749
K-smar <jessicayekini0@gmail.com>
2022-09-10 09:05:23 -0700

    .c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
-#include <studio.h>
-#include <studio.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * *main - Entry point
+ * *
+ * *Description: "and that piece of art is useful"
+ * "-Dora Korpar, 2015-10-19"
+ * *
+ *  *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
