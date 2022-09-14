aa328bfc71a40b31ad5a926d318700c2f20da86e
K-smar <jessicayekini0@gmail.com>
2022-09-14 07:04:27 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-```int _putchar(char c);
+#define HOLBERTON_H
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -9,4 +10,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);```
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
