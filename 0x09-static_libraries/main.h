2f9564108d5d035440f996fa8abfedf5bc30f81b
K-smar <jessicayekini0@gmail.com>
2022-10-01 16:04:08 -0700

    static

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -25,8 +25,8 @@ int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-unsigned int_strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
 
 #endif
