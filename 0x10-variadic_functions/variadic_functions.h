8540b3568c6544d52f089eab75142cd87a0b35e8
K-smar <jessicayekini0@gmail.com>
2022-10-13 09:04:37 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,10 +3,10 @@
 
 #include <stdarg.h>
 /**
-*struct print - print type with corresponding print function
-*@t: print type
-*@f: print function
-*/
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
 typedef struct print
 {
 	char *t;
@@ -14,7 +14,8 @@ typedef struct print
 } print_t;
 
 int _putchar(char);
-int sum_them_all(const unsigned int n, ...);void print_numbers(const char *separator, const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
