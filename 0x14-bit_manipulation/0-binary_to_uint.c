1c19d9f37803fe92ca729ded658aea9d14963d8e
K-smar <jessicayekini0@gmail.com>
2022-10-27 22:13:47 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,46 +1,36 @@
-#include <stddef.h>
 #include "main.h"
 
 /**
- * check_valid_string - checks if a string has only 0's and 1's
- * @b: string to be checked
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * Return: 1 if string is valid, 0 otherwise
- */
-int check_valid_string(const char *b)
-{
-	if (b == NULL)
-		return (0);
-	while (*b)
-	{
-		if (*b != '1' && *b != '0')
-			return (0);
-		b++;
-	}
-
-	return (1);
-}
-
-
-/**
- * binary_to_uint - converts a binary to an unsigned int
- * @b: the binary number as a string
- *
- * Return: the converted value
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int str_len = 0, base = 1;
+	unsigned int ui;
+	int len, base_two;
 
-	if (!check_valid_string(b))
+	if (!b)
 		return (0);
-	while (b[str_len] != '\0')
-		str_len++;
-	while (str_len)
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = l; len >= 0; len--, base_two *= 2)
 	{
-		decimal += ((b[str_len - 1] - '0') * base);
-		base *= 2;
-		str_len--;
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
+
+	return (ui);
 }
