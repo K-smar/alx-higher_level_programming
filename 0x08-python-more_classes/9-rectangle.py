14a74ac6f2552072cdf494a5247051161364eb43
K-smar <jessicayekini0@gmail.com>
2023-01-02 23:19:32 +0000

    task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -78,6 +78,15 @@ class Rectangle:
             return (rect_1)
         return (rect_2)
 
+    @classmethod
+    def square(cls, size=0):
+        """Return a new Rectangle with width and height equal to size.
+
+        Args:
+            size (int): The width and height of the new Rectangle.
+        """
+        return (cls(size, size))
+
     def __str__(self):
         """Return the printable representation of the Rectangle.
 
