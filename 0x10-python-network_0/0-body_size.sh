70f844d397ae85d3e499cf304fd1771665cb61da
K-smar <jessicayekini0@gmail.com>
2023-03-31 08:58:10 +0000

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This program takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-curl -sI "$1" | grep "Content-Length:" |
+# ends a request to that URL displays the size of the response body
+curl -sI "$1" | grep -i Content-Length
--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-s program takes in a URL, sends a GET request to the URL, and displays the body of the response.
-curl -sL "$1" -X GET -D ./header -o ./output; if grep -q "200 OK" ./header; then cat ./output; fi
-
