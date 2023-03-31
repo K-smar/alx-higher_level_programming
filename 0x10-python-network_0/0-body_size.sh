094f0d4bcf3e8b996b9347ee38b7def0afb26e2a
K-smar <jessicayekini0@gmail.com>
2023-03-31 20:52:34 +0000

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,1 +1,3 @@
-#!/bin/bash# Bash script that takes in a URL, sends a request to that URLcurl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
+#!/bin/bash
+# Bash script that takes in a URL, sends a request to that URL
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
--- 0x10-python-network_0/0x10-python-network_0/0-body_size.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL
-curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
